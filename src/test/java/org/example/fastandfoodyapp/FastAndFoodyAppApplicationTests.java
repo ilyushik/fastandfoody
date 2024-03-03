c9917103d29e8be45db234318162daf288a0510b
ilyushik <kamaraliilya@gmail.com>
2024-03-03 19:00:17 +0100

    project structure

---
+++ src/test/java/org/example/fastandfoodyapp/FastAndFoodyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fastandfoodyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastAndFoodyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
