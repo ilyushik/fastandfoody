df8a026c03369e266fceee719e89d5229c682aa0
ilyushik <kamaraliilya@gmail.com>
2024-03-09 10:04:58 +0100

    mail sender

---
+++ src/main/java/org/example/fastandfoodyapp/Mails/MailService.java
@@ -0,0 +1,26 @@
+package org.example.fastandfoodyapp.Mails;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromMail;
+
+    public void sendMail(String email, MailStructure mailStructure) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(fromMail);
+        simpleMailMessage.setSubject(mailStructure.getSubject());
+        simpleMailMessage.setText(mailStructure.getMessage());
+        simpleMailMessage.setTo(email);
+        javaMailSender.send(simpleMailMessage);
+    }
+
+}
