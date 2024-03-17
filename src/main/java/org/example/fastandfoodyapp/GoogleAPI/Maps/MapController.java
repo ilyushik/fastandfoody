41f7d57ced8c26e63b486246f90870ec1b3d014a
ilyushik <kamaraliilya@gmail.com>
2024-03-17 20:56:24 +0100

    added main map and detail map for each restaurant

--- src/main/java/org/example/fastandfoodyapp/GoogleAPI/Maps/MapController.java
+++ src/main/java/org/example/fastandfoodyapp/GoogleAPI/Maps/MapController.java
@@ -3,8 +3,13 @@ package org.example.fastandfoodyapp.GoogleAPI.Maps;
 import lombok.AllArgsConstructor;
 import org.example.fastandfoodyapp.GoogleAPI.Maps.Service.MapService;
 import org.example.fastandfoodyapp.GoogleAPI.Maps.Service.MapServiceImpl;
+import org.example.fastandfoodyapp.Model.DTO.RestaurantDTO;
+import org.example.fastandfoodyapp.Model.Restaurant;
+import org.example.fastandfoodyapp.Repositories.RestaurantRepository;
+import org.example.fastandfoodyapp.Services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +19,20 @@ import java.util.List;
 public class MapController {
 
     MapService mapService;
+    private RestaurantRepository restaurantRepository;
 
     @GetMapping("/maps")
     public List<MarkerDTO> getMarkers() {
         return mapService.getAllMarkerDTO();
+    }
+
+    @GetMapping("/maps/{id}")
+    public MarkerDTO showMarkerById(@PathVariable("id") int id) {
+        Restaurant restaurant = restaurantRepository.findById(id).orElseThrow();
+        MarkerDTO markerDTO = new MarkerDTO();
+        markerDTO.setAddress(restaurant.getAddress());
+        markerDTO.setLatitude(restaurant.getLatitude());
+        markerDTO.setLongitude(restaurant.getLongitude());
+        return markerDTO;
     }
 }
