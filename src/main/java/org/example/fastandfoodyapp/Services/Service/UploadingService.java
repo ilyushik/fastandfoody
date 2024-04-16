85f76bb52b4a1293064d5029d302e838f21ef0d6
IlliaKyselov1902 <qwertytwenty129@gmail.com>
2024-04-16 22:59:19 +0300

    Added PayPal workflow

--- src/main/java/org/example/fastandfoodyapp/Services/Service/UploadingService.java
+++
@@ -1,7 +0,0 @@
-package org.example.fastandfoodyapp.Services.Service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadingService {
-    public void uploadProfileImage(int accountId, MultipartFile file);
-}
